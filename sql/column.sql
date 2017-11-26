772b049370f10f5abf7434d3915a8411b6d98732
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2017-11-26 23:35:51 +0900

    Fix column.sql

--- sql/column.sql
+++ sql/column.sql
@@ -2,4 +2,4 @@
 -- select non deleted users
 -- column "password" should not be contained
 
-select * from user where (not deleted_at is null) and (not password like "");
+select * from user where (deleted_at is null) and (not password like "");
