c9646500381355aefdb4e0409bae4e7494eead14
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2017-11-26 23:38:35 +0900

    Update column.sql

--- sql/column.sql
+++ sql/column.sql
@@ -2,4 +2,6 @@
 -- select non deleted users
 -- column "password" should not be contained
 
-select * from user where (deleted_at is null) and (not password like "");
+-- パスワードを持たない...？
+-- DB のパターンから 100件中, 88 件のパスワードを持たないレコードがどれか判断できなかった
+select * from user where (deleted_at is null) and (password like "");
